ee45bb3787663d0f1afa914ddcfad4e1ba7c4dd1
Programador Cego <programadorcego@gmail.com>
2026-03-01 09:15:57 -0300

    URL e Métodos

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 import http from "node:http";
 
+/*
+ Métodos
+
+ GET: Buscar alguma coisa
+ POST: Criar alguma coisa
+ PUT: Atualizar completamente
+ PATCH: Atualizar parcialmente
+ DELETE: Remover completamente
+*/
+
 const server = http.createServer((req, res) => {
-    res.end("Olá, mundo");
+    if (req.url === "/users" && req.method === "GET") {
+        return res.end("Listar usuários");
+    }
+
+    if (req.url === "/users" && req.method === "POST") {
+        return res.end("Criar usuário");
+    }
 });
 
 server.listen(3333, "localhost", () => {
