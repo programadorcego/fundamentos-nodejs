fd58990735bb8d6c79e47885b96a1b676195484d
Programador Cego <programadorcego@gmail.com>
2026-05-21 07:11:59 -0300

    Persistindo banco de dados

--- src/database.js
+++ src/database.js
@@ -1,4 +1,22 @@
+import fs from "node:fs/promises";
+
+const databasePath = new URL("../db.json", import.meta.url);
+
 export class Database {
+    constructor() {
+        fs.readFile(databasePath)
+            .then(data => {
+                this.#database = JSON.parse(data);
+            })
+            .catch(() => {
+                this.#persist();
+            });
+    }
+
+    #persist() {
+        fs.writeFile(databasePath, JSON.stringify(this.#database));
+    }
+
     #database = {};
 
     select(table) {
@@ -13,6 +31,8 @@ export class Database {
         } else {
             this.#database[table] = [data];
         }
+
+        this.#persist();
 
         return data;
     }
